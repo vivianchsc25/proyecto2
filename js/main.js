f37646486a59a406f5bea74c887e136e56a2e44e
vivianchsc25 <vivith.vs@gmail.com>
2023-08-05 18:58:09 -0300

    lovely

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 
-const menuBtn = document.querySelector('#menu-btn')
 
-console.log(menuBtn);
+//menu hamburguesa
+const menuBtn = document.querySelector('#menu-btn')
+// console.log(menuBtn);
 menuBtn.addEventListener('click',function() {
     document.body.classList.toggle('mobile-menu-active')
     
 })
+
+
+// hoover
+
+const menuItems = document.querySelectorAll ('.menu-item')
+
+menuItems.forEach (function(item) {
+    item.addEventListener('click', function(e){
+        const activeItem = document.querySelector('.active');
+        activeItem.classList.remove('active');
+        e.target.classList.add('active');
+    });
+    
+});
